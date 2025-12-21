898504b4d4153913ee7595920c0e50c370c3d7fb
Glen Marchesani <glen@accur8.net>
2025-12-20 19:29:43 -0500

    Skip tests during nix build

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
-  description = "Herman - A launcher for Java applications";
+  description = "Herman - a builder for nix based java app launchers";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -10,25 +10,33 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        packages.default = pkgs.buildGoModule {
+
+        herman = pkgs.buildGoModule {
           pname = "herman";
           version = "0.1.0";
           src = ./.;
           vendorHash = null;
           subPackages = [ "src" ];
 
+          # Skip tests that require nix in the build environment
+          doCheck = false;
+
           postInstall = ''
             mv $out/bin/src $out/bin/herman
           '';
 
           meta = with pkgs.lib; {
-            description = "A launcher for Java applications";
+            description = "Herman - a builder for nix based java app launchers";
             homepage = "https://github.com/accur8/herman";
             license = licenses.mit;
             maintainers = [ ];
           };
+        };
+      in
+      {
+        packages = {
+          default = herman;
+          herman = herman;
         };
 
         devShells.default = pkgs.mkShell {
